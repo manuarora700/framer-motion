f3b94b02fd76be25d78377168f926bdb5791d8e0
Manu Arora <mannuarora700@gmail.com>
2021-04-10 20:11:34 +0530

    ⚡️ Div motion, pretty easy

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div
+      className="home container"
+      animate={{ marginTop: 200, opacity: 0.2, rotateZ: 180 }}
+    >
+      <motion.h2 animate={{ fontSize: 50, color: "#ff2994", x: 10, y: 10 }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button animate={{ scale: 1.5 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
