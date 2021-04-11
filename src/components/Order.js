4d22c3f244354c4914292683a9b31ba2a3159d74
Manu Arora <mannuarora700@gmail.com>
2021-04-11 19:59:42 +0530

    ⚡️ <AnimatePresence> for exit animatons

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import { motion, AnimatePresence } from "framer-motion";
+import React, { useState } from "react";
+
+const containerVariant = {
+  hidden: { opacity: 0, x: "-100vw" },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { type: "spring", delay: 0.5 },
+  },
+};
+
+const childVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 
 const Order = ({ pizza }) => {
+  const [showTitle, setShowTitle] = useState(true);
+
+  setTimeout(() => {
+    setShowTitle(false);
+  }, 4000);
+
   return (
-    <div className="container order">
-      <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+    <motion.div
+      className="container order"
+      variants={containerVariant}
+      initial="hidden"
+      animate="visible"
+    >
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2 exit={{ opacity: 0 }}>
+            Thank you for your order :)
+          </motion.h2>
+        )}
+      </AnimatePresence>
+      <motion.p variants={childVariant} initial="hidden" animate="visible">
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childVariant}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
 
 export default Order;
